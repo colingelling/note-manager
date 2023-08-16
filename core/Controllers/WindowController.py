813fe813be528ea7b41e532320d762c9d15be59c
Colin Gelling <22787309+colingelling@users.noreply.github.com>
2023-08-16 10:26:48 +0200

    Removed multiple parts of the application because of following MVP concepts. Don't need user registration and login for now.

--- core/Controllers/WindowController.py
+++ core/Controllers/WindowController.py
@@ -1,6 +1,6 @@
 """
 
-    Created by Colin Gelling on 26/4/2023
+    Created by Colin Gelling on 26/04/2023
     Using Pycharm Professional
 
 """
@@ -18,50 +18,6 @@ class WindowController:
             WindowController.active_window.close()
 
         home_window = HomeView()
-        home_window.switch_register.connect(WindowController.show_register_window)
-        home_window.switch_login.connect(WindowController.show_login_window)
         home_window.show()
 
         WindowController.active_window = home_window
-
-    @staticmethod
-    def show_register_window():
-        from views.RegisterView import RegisterView
-
-        if WindowController.active_window:
-            WindowController.active_window.close()
-
-        register_window = RegisterView()
-        register_window.switch_home.connect(WindowController.show_home_window)
-        register_window.switch_login.connect(WindowController.show_login_window)
-        register_window.show()
-
-        WindowController.active_window = register_window
-
-    @staticmethod
-    def show_login_window():
-        from views.LoginView import LoginView
-
-        if WindowController.active_window:
-            WindowController.active_window.close()
-
-        login_window = LoginView()
-        login_window.switch_home.connect(WindowController.show_home_window)
-        login_window.switch_register.connect(WindowController.show_register_window)
-        login_window.switch_user.connect(WindowController.show_user_window)
-
-        login_window.show()
-
-        WindowController.active_window = login_window
-
-    @staticmethod
-    def show_user_window():
-        from views.UserView import UserView
-
-        if WindowController.active_window:
-            WindowController.active_window.close()
-
-        user_window = UserView()
-        user_window.show()
-
-        WindowController.active_window = user_window
