20ce4e5d3ae266ee3948e7be2c61518cc4d4206f
Colin Gelling <22787309+colingelling@users.noreply.github.com>
2024-04-22 11:19:25 +0200

    Added taskdescriptions

--- core/Managers/TreeView/ItemManager.py
+++ core/Managers/TreeView/ItemManager.py
@@ -30,7 +30,6 @@ class ItemManager:
         
         # Iteration through both lists using one as key, the other as value in order to connect the action functions
         for action, function in zip(actions, functions):
-            # TODO: Pass
             action.triggered.connect(partial(function, index.data(Qt.ItemDataRole.DisplayRole)))
             
         # Execute the menu
@@ -58,18 +57,32 @@ class ItemManager:
         print(f"Delete action triggered on item: '{item}' \n")
         
         ItemManager.show_warning(item)
+        
+        # TODO:
+        #  1) Find out on how to spot differences between TreeView -items
+        #  (Maybe comparing 'item' with data from the get_notebook_information model)
+        #  2) Do nothing if the item is a note, continue when it is a notebook
+        #  (Expand on the warning dialog, add delete functionality behind the 'Yes' button)
     
     @staticmethod
     def _item_edit(item):
         print(f"Edit action triggered on item: '{item}'")
         
+        # TODO:
+        #  1) Find out on how to spot differences between TreeView -items
+        #  (Maybe comparing 'item' with data from the get_notebook_information model)
+        #  2) Do nothing if the item is a note, continue when it is a notebook
+        #  3) In this case, add and open a new dialog window start throwing some logic to it in order to open the
+        #  notebook for being able to change the name of it
+        
     @staticmethod
-    def show_warning(item):
+    def show_warning(item):  # TODO: Move somewhere else
         warning_box = QMessageBox()
         warning_box.setIcon(QMessageBox.Icon.Warning)
         warning_box.setText("Are you sure you want to delete this item?")
         warning_box.setInformativeText(f"You are about to delete the following item: '{item}'. \n\n"
                                        f"Are you sure? This action cannot be undone!")
+        
         warning_box.setStandardButtons(QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
         warning_box.setDefaultButton(QMessageBox.StandardButton.No)
         
